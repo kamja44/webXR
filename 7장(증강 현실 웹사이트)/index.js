2ab426709c6ecd722f629003bc5ef013dadf975c
kamja44 <88872409+kamja44@users.noreply.github.com>
2023-05-11 17:51:58 +0900

    7장

---
+++ 7장(증강 현실 웹사이트)/index.js
@@ -0,0 +1,32 @@
+import * as THREE from "./node_modules/three/build/three.module/js";
+// 전역 장면 값
+var btn, gl, glCanvas, camera, scene, renderer, cube;
+// 전역 xr 값
+var xrSession = null;
+function loadScene() {
+  // WebGL 콘텍스트와 Three.js 장면의 구성 요소 설정
+}
+function init() {
+  // 스크립트 실행 시작
+}
+function onRequestSession() {
+  // XR 세션 요청 처리
+}
+function onSessionStarted() {
+  // XR 세션이 생성되면 처리
+}
+function setupWebGLLayer() {
+  // WebGL 콘텍스트를 XR 세션에 연결
+}
+function animate() {
+  // 애니메이션 루프 시작
+}
+function render(time) {
+  // GPU에 그리기 명령 실행
+}
+function endXRSession() {
+  // XR 세션 종료
+}
+function onSessionEnd() {
+  // XR 세션의 종료 이벤트 처리
+}
